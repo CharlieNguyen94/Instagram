1f784ac79bb4dcced062abad6392e0874729cbaf
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-05-26 13:58:10 +0100

    Sign up successfully connected with Firebase.

--- Instagram/Controllers/Others/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Others/Onboarding/LoginViewController.swift
@@ -5,45 +5,117 @@
 //  Created by Charlie Nguyen on 25/05/2021.
 //
 
+import SafariServices
 import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
     private let usernameEmailField: UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .continue
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        
         return field
     }()
     
     private let loginButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Log in", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        
+        return button
     }()
     
     private let termsButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Terms of Service", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
+        return button
     }()
     
     private let privacyButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Privacy Policy", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
+        return button
     }()
     
     private let createAccountButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New user? Create an Account", for: .normal)
+        return button
     }()
     
     private let headerView: UIView = {
-        return UIView()
+        let header = UIView()
+        header.clipsToBounds = true
+        let backgroundImageView = UIImageView(image: UIImage(named: "gradient"))
+        header.addSubview(backgroundImageView)
+        return header
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loginButton.addTarget(
+            self,
+            action: #selector(didTapLoginButton),
+            for: .touchUpInside)
+        
+        createAccountButton.addTarget(
+            self,
+            action: #selector(didTapCreateAccountButton),
+            for: .touchUpInside)
+        
+        termsButton.addTarget(
+            self,
+            action: #selector(didTapTermsButton),
+            for: .touchUpInside)
+        
+        privacyButton.addTarget(
+            self,
+            action: #selector(didTapPrivacyButton),
+            for: .touchUpInside)
+        
+        usernameEmailField.delegate = self
+        passwordField.delegate = self
+        
         addSubViews()
-
+        
         view.backgroundColor = .systemBackground
     }
     
@@ -51,6 +123,71 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         // Assign frames
+        
+        headerView.frame = CGRect(
+            x: 0,
+            y: 0.0,
+            width: view.width,
+            height: view.height / 3.0)
+        
+        usernameEmailField.frame = CGRect(
+            x: 25,
+            y: headerView.bottom + 40,
+            width: view.width - 50,
+            height: 52)
+        
+        passwordField.frame = CGRect(
+            x: 25,
+            y: usernameEmailField.bottom + 10,
+            width: view.width - 50,
+            height: 52)
+        
+        loginButton.frame = CGRect(
+            x: 25,
+            y: passwordField.bottom + 10,
+            width: view.width - 50,
+            height: 52)
+        
+        createAccountButton.frame = CGRect(
+            x: 25,
+            y: loginButton.bottom + 10,
+            width: view.width - 50,
+            height: 52)
+        
+        termsButton.frame = CGRect(
+            x: 10,
+            y: view.height - view.safeAreaInsets.bottom - 100,
+            width: view.width - 20,
+            height: 50)
+        
+        privacyButton.frame = CGRect(
+            x: 10,
+            y: view.height - view.safeAreaInsets.bottom - 50,
+            width: view.width - 20,
+            height: 50)
+        
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else { return }
+        
+        // Add instagram logo
+        
+        guard let backgroundView = headerView.subviews.first else {
+            return
+        }
+        
+        backgroundView.frame = headerView.bounds
+        
+        // Add Instagram logo
+        let imageView = UIImageView(image: UIImage(named: "text"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: headerView.width / 4.0,
+                                 y: view.safeAreaInsets.top,
+                                 width: headerView.width / 2.0,
+                                 height: headerView.height - view.safeAreaInsets.top)
     }
     
     private func addSubViews() {
@@ -64,20 +201,76 @@ class LoginViewController: UIViewController {
     }
     
     @objc func didTapLoginButton() {
+        passwordField.resignFirstResponder()
+        usernameEmailField.resignFirstResponder()
+        
+        guard let usernameEmail = usernameEmailField.text, !usernameEmail.isEmpty,
+              let password = passwordField.text, !password.isEmpty, password.count >= 8 else { return }
+        
+        var username: String?
+        var email: String?
+        
+        // Login functionality
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            // Email
+            email = usernameEmail
+            
+        } else {
+            // Username
+            username = usernameEmail
+        }
+        
+        
+        AuthManager.shared.loginUser( username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    // User logged in
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    // Error occured
+                    let alert = UIAlertController(title: "Log In Error",
+                                                  message: "We were able to log you in",
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+        }
+    }
+    
+    @objc func didTapTermsButton() {
+        guard let url = URL(string: "https://help.instagram.com/581066165581870/?helpref=hc_fnav&bc[0]=Instagram%20Help&bc[1]=Policies%20and%20Reporting") else { return }
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
         
     }
     
     @objc func didTapPrivacyButton() {
-        
-    }
-    
-    @objc func didTapTermsButton() {
-        
+        guard let url = URL(string: "https://help.instagram.com/519522125107875/?helpref=hc_fnav&bc[0]=Instagram%20Help&bc[1]=Policies%20and%20Reporting") else { return }
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
     }
     
     @objc func didTapCreateAccountButton() {
+        let vc = RegistrationViewController()
+        vc.title = "Create Account"
+        
+        present(UINavigationController(rootViewController: vc), animated: true)
         
     }
     
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameEmailField {
+            passwordField.becomeFirstResponder()
+            
+        }
+        else if textField == passwordField {
+            didTapLoginButton()
+        }
+        
+        return true
+    }
 }
--- Instagram/Resources/DatabaseManager.swift
+++ Instagram/Resources/DatabaseManager.swift
@@ -11,5 +11,35 @@ public class DatabaseManager {
     
     static let shared = DatabaseManager()
     
+    private let database = Database.database(url: "https://instagram-6691a-default-rtdb.europe-west1.firebasedatabase.app").reference()
+    
+    // MARK: - Public
+    
+    /// Check if username and email is available
+    /// - Parameters
+    /// - email: String representing email
+    /// - username: String representing username
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
+    
+    /// Inserts new user to database
+    /// - Parameters
+    /// - email: String representing email
+    /// - username: String representing username
+    /// - completion: Async callback for result if database entry succeeded
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                // Succeeded
+                completion(true)
+                return
+            } else {
+                // Failed
+                completion(false)
+                return
+            }
+        }
+    }
     
 }
